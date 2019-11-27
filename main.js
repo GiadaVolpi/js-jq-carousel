be5f04234c0c7e67cb031480f359d8f4221a136e
Gi@da <giadavolpi8@gmail.com>
2019-11-27 18:12:25 +0100

    booohh

--- main.js
+++ main.js
@@ -9,17 +9,33 @@ $ (document).ready (function () {
         // prendo l'immagine successiva a quella attualmente visibile
         var imgSuccessiva = imgAttuale.next ("img");
 
+        // prendo il cerchio attivo (deve avere il pieno visibile e il vuoto invisibile)
+        var cerchioAttivo = $ ("#circle .pieno.circle-active #circle .vuoto.circle.disactive");
+
+        // prendo il cerchio successivo a quello attivo
+        var cerchioSuccessivo = cerchioAttivo.next ();
+
         // rendo invisibile la foto attuole
         imgAttuale.removeClass ("active");
+
+        // disattivo il cerchio attivo
+        $ (".pieno.circle-active") cerchioAttivo.removeClass ("circle-active");
+        $ (".vuoto.circle-disactive") cerchioAttivo.removeClass ("circle-disactive");
 
         // se l'immagine successiva non esiste
         if (imgSuccessiva.length == 0) {
             // rendo nuovamente visibile la prima del ciclo
             imgSuccessiva = $ (".restart");
+
+            $ (".circle-first .pieno") cerchioSuccessivo.addClass ("circle-active");
+            $ (".circle-first .vuoto") cerchioSuccessivo.addClass ("circle.disactive");
         }
 
         // rendo visibile la foto successiva
         imgSuccessiva.addClass ("active");
+
+        $ (".pieno") cerchioSuccessivo.addClass ("circle-active")
+        $ (".vuoto") cerchioSuccessivo.addClass ("circle.disactive")
     });
 
 
